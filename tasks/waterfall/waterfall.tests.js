fa04db0e43a7204e8153178df37b3aae70507dfb
RubaXa <ibnRubaXa@gmail.com>
2014-08-27 08:50:49 +0400

    + waterfall

---
+++ tasks/waterfall/waterfall.tests.js
@@ -0,0 +1,24 @@
+({
+	"sync": function () {
+		return new Promise(function (resolve, reject) {
+			waterfall(
+				function () { return 10; },
+				function (val) { return val + 5; },
+				function (val) { resolve([15, val]); }
+			);
+			setTimeout(reject, 1);
+		});
+	},
+
+	"async": function () {
+		return new Promise(function (resolve, reject) {
+			waterfall(
+				function () { return 10; },
+				function (val, next) { setTimeout(function () { next(val + 5); }, 10); },
+				function (val) { return val * 2; },
+				function (val) { resolve([30, val]); }
+			);
+			setTimeout(reject, 50);
+		});
+	}
+})
