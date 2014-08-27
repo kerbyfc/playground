31e9519c1c2a48a66d6839dba8def39217292b1c
RubaXa <ibnRubaXa@gmail.com>
2014-08-27 09:50:08 +0400

    + parallel

---
+++ tasks/parallel/parallel.tests.js
@@ -0,0 +1,29 @@
+({
+	"sync": function () {
+		return new Promise(function (resolve, reject) {
+			parallel([
+				function (resolve) { resolve(10); },
+				function () { return 5; },
+				function (resolve) { resolve(0); }
+			], function (results) {
+				resolve([[10, 5, 0], results]);
+			});
+
+			setTimeout(reject, 1);
+		});
+	},
+
+	"async": function () {
+		return new Promise(function (resolve, reject) {
+			parallel([
+				function (resolve) { setTimeout(function () { resolve(10); }, 50); },
+				function () { return 5; },
+				function (resolve) { setTimeout(function () { resolve(0); }, 10) }
+			], function (results) {
+				resolve([[10, 5, 0], results]);
+			});
+
+			setTimeout(reject, 100);
+		});
+	}
+})
